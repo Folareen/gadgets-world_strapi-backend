5e5222c3d7f0eca44487f353023bfb7be54fe823
Folareen <sakawahab03@gmail.com>
2022-11-20 00:32:33 +0100

    config for deployment on railway

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,11 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const path = require("path");
 
-module.exports = ({ env }) => ({
+module.exports = () => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
